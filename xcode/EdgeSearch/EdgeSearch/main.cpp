71b18098231b6c351da9006112b5ad95f54a00a2
jimmy <jianhui.chenn@gmail.com>
2018-11-05 11:06:03 -0800

    rename cvx_gl and cvx_pgl

---
+++ xcode/EdgeSearch/EdgeSearch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EdgeSearch
+//
+//  Created by jimmy on 11/5/18.
+//  Copyright (c) 2018 Nowhere Planet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
